21517a1e44ec48f247767081414ba9daeee9528c
yunusmujadidi <yunusmujadidi@gmail.com>
2025-12-17 23:47:25 +0700

    feat: add global validation pipe and update create task dto

--- apps/backend/src/task/dto/create-task.dto.ts
+++ apps/backend/src/task/dto/create-task.dto.ts
@@ -1,10 +1,32 @@
+import {
+  IsDateString,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 import { Status } from 'generated/prisma/enums';
 
 export class CreateTaskDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3, { message: 'Min length for title is 3' })
+  @MaxLength(60, { message: 'Max length for title is 60' })
   title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3, { message: 'Min length for description is 3' })
+  @MaxLength(160, { message: 'Max length for description is 160' })
   description: string;
-  status: Status;
+
+  @IsOptional()
+  @IsEnum(Status)
+  status?: Status;
+
+  @IsNotEmpty()
+  @IsDateString()
   deadline: string;
-  userId: string;
-  createdBy: string;
 }
