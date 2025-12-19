62ed8c4bec0cee6455706e4b795ad69d0cc5fe4a
yunusmujadidi <yunusmujadidi@gmail.com>
2025-12-19 13:12:54 +0700

    fix: bug on api not working

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,6 +6,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // cors for frontend
+  // TODO: maybe add frontend env
+  app.enableCors();
+
   // global validation pipes
   app.useGlobalPipes(new ValidationPipe());
 
