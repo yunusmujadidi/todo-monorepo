bf90c60a3a25e61d6063ba3811cc34c4cd202a08
yunusmujadidi <yunusmujadidi@gmail.com>
2025-12-19 14:10:01 +0700

    feat: display task on data table and add action button on title

--- apps/frontend/src/components/layout/title.tsx
+++ apps/frontend/src/components/layout/title.tsx
@@ -1,3 +1,7 @@
+"use client";
+import { useTaskModal } from "@/hooks/use-task";
+import { Button } from "../ui/button";
+
 export const Title = ({
   title,
   description,
@@ -5,12 +9,14 @@ export const Title = ({
   title: string;
   description: string;
 }) => {
+  const { openCreate } = useTaskModal();
   return (
     <div className="flex justify-between">
       <div>
         <h1 className="text-3xl font-semibold">{title}</h1>
         <p className="text-muted-foreground">{description}</p>
       </div>
+      <Button onClick={() => openCreate()}>Add new Task</Button>
     </div>
   );
 };
