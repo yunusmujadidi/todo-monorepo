1080c7f81e0e0656fd775e24500f2224363fb163
yunusmujadidi <yunusmujadidi@gmail.com>
2025-12-19 10:30:14 +0700

    feat: add auth pages

--- apps/frontend/src/app/(auth)/(routes)/sign-in/page.tsx
+++ apps/frontend/src/app/(auth)/(routes)/sign-in/page.tsx
@@ -1,5 +1,43 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+
+// TODO: add sign in form
 const SignInPage = () => {
-  return <div>sign in page</div>;
+  return (
+    // container
+    <div className="flex w-full h-screen items-center justify-center">
+      <Card className="w-full max-w-sm">
+        <CardHeader className="border-b">
+          <CardTitle>Sign in before using the app</CardTitle>
+          <CardDescription>
+            Fill this form to sign in to the app
+          </CardDescription>
+        </CardHeader>
+        <CardContent>sign in form</CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full">
+            Sign In
+          </Button>
+          <div className="mt-6">
+            <p className="text-sm ">
+              Don&apos;t have an account?
+              <Link className="ml-1 hover:underline" href="/sign-up">
+                Sign up
+              </Link>
+            </p>
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 };
 
 export default SignInPage;
--- apps/frontend/src/app/(auth)/(routes)/sign-up/page.tsx
+++ apps/frontend/src/app/(auth)/(routes)/sign-up/page.tsx
@@ -1,5 +1,43 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+
+// TODO: add sign up form
 const SignUpPage = () => {
-  return <div>sign up page</div>;
+  return (
+    // container
+    <div className="flex w-full h-screen items-center justify-center">
+      <Card className="w-full max-w-sm">
+        <CardHeader className="border-b">
+          <CardTitle>Sign up to create an account</CardTitle>
+          <CardDescription>
+            Fill this form to sign up to the app
+          </CardDescription>
+        </CardHeader>
+        <CardContent>sign iup form</CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full">
+            Sign Up
+          </Button>
+          <div className="mt-6">
+            <p className="text-sm ">
+              Already have an account?
+              <Link className="ml-1 hover:underline" href="/sign-in">
+                Sign in
+              </Link>
+            </p>
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 };
 
 export default SignUpPage;
